c256f5d1a13903591ff23ce928b9fdaec2ab1ebd
anoov <1374206329@qq.com>
2020-03-15 18:59:10 +0800

    add first question

--- 03-findRepeatNumber.h
+++ 03-findRepeatNumber.h
@@ -5,4 +5,19 @@
 #ifndef LEETCODE_03_FINDREPEATNUMBER_H
 #define LEETCODE_03_FINDREPEATNUMBER_H
 
+int findRepeatNumber(std::vector<int>& nums) {
+    int length = nums.size();
+    for (int i = 0; i < length; i++) {
+        while (nums[i] != i) {
+            if (nums[i] == nums[nums[i]])
+                return nums[i];
+            // auto tmp = nums[i];
+            // nums[i] = nums[tmp];
+            // nums[tmp] = tmp;
+            std::swap(nums[i], nums[nums[i]]);
+        }
+    }
+    return -1;
+}
+
 #endif //LEETCODE_03_FINDREPEATNUMBER_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <vector>
+#include "03-findRepeatNumber.h"
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<int> a{1, 3, 3, 2, 1};
+    cout << findRepeatNumber(a) << endl;
     return 0;
 }
