6431e98c9758435af78242a98cfdd2cc648d2e1b
anoov <1374206329@qq.com>
2020-03-17 16:55:09 +0800

    改变目录结构

---
+++ algorithm/04-findNumberIn2DArray.h
@@ -0,0 +1,28 @@
+//
+// Created by 孙卓文 on 2020/3/17.
+//
+
+#ifndef LEETCODE_04_FINDNUMBERIN2DARRAY_H
+#define LEETCODE_04_FINDNUMBERIN2DARRAY_H
+
+bool findNumberIn2DArray(vector<vector<int>>& matrix, int target) {
+    if (matrix.empty()) return false;
+    int rows = matrix.size();
+    int cols = matrix[0].size();
+    int row = 0;
+    int col = cols - 1;
+    while (row < rows && col >= 0) {
+        int tmp = matrix[row][col];
+        if (target > tmp) {
+            row++;
+        }
+        else if (target < tmp) {
+            col--;
+        }
+        else {
+            return true;
+        }
+    }
+    return false;
+}
+#endif //LEETCODE_04_FINDNUMBERIN2DARRAY_H
--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <vector>
-#include "algorithm/03-findRepeatNumber.h"
-#include "algorithm/11-minArray.h"
-#include "algorithm/12-exist.h"
-#include "algorithm/13-movingCount.h"
-#include "algorithm/14-cuttingRope.h"
-#include "algorithm/15-hammingWeight.h"
-#include "algorithm/16-myPow.h"
-#include "algorithm/17-printNumbers.h"
-#include "algorithm/19-isMatch.h"
-#include "algorithm/20-isNumber.h"
-#include "algorithm/21-exchange.h"
-#include "algorithm/22-getKthFromEnd.h"
-#include "algorithm/24-reverseList.h"
-#include "algorithm/25-mergeTwoLists.h"
 using namespace std;
+//#include "algorithm/03-findRepeatNumber.h"
+//#include "algorithm/11-minArray.h"
+//#include "algorithm/12-exist.h"
+//#include "algorithm/13-movingCount.h"
+//#include "algorithm/14-cuttingRope.h"
+//#include "algorithm/15-hammingWeight.h"
+//#include "algorithm/16-myPow.h"
+//#include "algorithm/17-printNumbers.h"
+//#include "algorithm/19-isMatch.h"
+//#include "algorithm/20-isNumber.h"
+//#include "algorithm/21-exchange.h"
+//#include "algorithm/22-getKthFromEnd.h"
+//#include "algorithm/24-reverseList.h"
+//#include "algorithm/25-mergeTwoLists.h"
+#include "algorithm/04-findNumberIn2DArray.h"
 
 int main() {
 
